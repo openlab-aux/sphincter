d170e6048c3846ff025138cde3a492660e7c39ce
Michael <accounts@michiwend.com>
2014-01-17 15:53:33 +0100

    added lib folder

--- src/sphinkter.ino
+++ src/sphinkter.ino
@@ -254,14 +254,43 @@ void setup()  {
 
     // serial debugging
     Serial.begin(9600);
-    Serial.println("************************************************");
-    Serial.println("*                                              *");
-    Serial.println("*     Welcome to fu**ing awesome Sphinkter     *");
-    Serial.println("*                                              *");
-    Serial.println("*        send 'o', 'c' to open / close         *");
-    Serial.println("*        and 'r' to reset positioning          *");
-    Serial.println("*                                              *");
-    Serial.println("************************************************");
+    //Serial.println("************************************************");
+    //Serial.println("*                                              *");
+    //Serial.println("*     Welcome to fu**ing awesome Sphinkter     *");
+    //Serial.println("*                                              *");
+    //Serial.println("*        send 'o', 'c' to open / close         *");
+    //Serial.println("*        and 'r' to reset positioning          *");
+    //Serial.println("*                                              *");
+    //Serial.println("************************************************");
+    
+    Serial.println(" welcome to fucking awesome sphinkter!");
+    Serial.println("");        
+    Serial.println("  /     \\             \\            /    \\");
+    Serial.println(" |       |             \\          |      |");      
+    Serial.println(" |       `.             |         |       :");
+    Serial.println(" `        |             |        \\|       |");     
+    Serial.println("  \\       | /       /  \\\\\\   --__ \\\\       :");
+    Serial.println("   \\      \\/   _--~~          ~--__| \\     |");      
+    Serial.println("    \\      \\_-~                    ~-_\\    |");
+    Serial.println("     \\_     \\        _.--------.______\\|   |");    
+    Serial.println("       \\     \\______// _ ___ _ (_(__>  \\   |");
+    Serial.println("        \\   .  C ___)  ______ (_(____>  |  /");    
+    Serial.println("        /\\ |   C ____)/      \\ (_____>  |_/");
+    Serial.println("       / /\\|   C_____)       |  (___>   /  \\");    
+    Serial.println("      |   (   _C_____)\\______/  // _/ /     \\");
+    Serial.println("      |    \\  |__   \\\\_________// (__/       |");  
+    Serial.println("     | \\    \\____)   `----   --'             |");  
+    Serial.println("     |  \\_          ___\\       /_          _/ |"); 
+    Serial.println("    |              /    |     |  \\            |"); 
+    Serial.println("    |             |    /       \\  \\           |"); 
+    Serial.println("    |          / /    |         |  \\           |");
+    Serial.println("    |         / /      \\__/\\___/    |          |");
+    Serial.println("   |           /        |    |       |         |");
+    Serial.println("   |          |         |    |       |         |");
+    Serial.println("");
+    Serial.println("      send 'o', 'c' to open / close");
+    Serial.println("        and 'r' to reset positioning");
+    Serial.println("");
 
 
     // start the Ethernet connection and the server:
