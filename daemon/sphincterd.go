4e214a581357d04f0038b248feb64715349cecb9
Michael Wendland <accounts@michiwend.com>
2014-08-24 02:25:13 +0200

    Handle multiple open HTTP requests. Testing.
    the daemon now waits for incoming serial data and responds to all open
    HTTP connections in the order in which they were received.

--- daemon/sphincterd.go
+++ daemon/sphincterd.go
@@ -12,35 +12,40 @@ import (
 )
 
 const (
-	ACTION_OPEN  string = "o"
-	ACTION_CLOSE string = "c"
-	ACTION_STATE string = "s"
-	ACTION_REF   string = "r"
+	// Commands for sphincter
+	CMD_OPEN  string = "o"
+	CMD_CLOSE string = "c"
+	CMD_STATE string = "s"
+	CMD_REF   string = "r"
+
+	// Response codes from sphincter
+	RSP_OPEN     string = "OPEN"
+	RSP_UNLOCKED string = "UNLOCKED"
+	RSP_LOCKED   string = "LOCKED"
+	RSP_UNKNOWN  string = "UNKNOWN"
+	RSP_ACK      string = "ACK"
+
+	// Http GET actions
+	ACN_OPEN  string = "open"
+	ACN_CLOSE string = "close"
+	ACN_STATE string = "state"
 )
 
-const (
-	RESPONSE_OPEN     string = "OPEN"
-	RESPONSE_UNLOCKED string = "UNLOCKED"
-	RESPONSE_LOCKED   string = "LOCKED"
-	RESPONSE_UNKNOWN  string = "UNKNOWN"
-	RESPONSE_ACK      string = "ACK"
-)
+type Sphincter struct {
+	dev   string
+	speed int
 
-type SerialHandler struct {
-	port       string
-	speed      int
-	serial_con io.ReadWriteCloser
+	io.ReadWriteCloser
 }
 
-func (sh *SerialHandler) connect() bool {
-
-	// FIXME: flush buffer
+// connect to sphincter
+func (s *Sphincter) connect() bool {
 
 	var err error
 
-	sh.serial_con, err = serial.OpenPort(&serial.Config{
-		Name: sh.port,
-		Baud: sh.speed})
+	s.ReadWriteCloser, err = serial.OpenPort(&serial.Config{
+		Name: s.dev,
+		Baud: s.speed})
 
 	if err != nil {
 		log.Println(err)
@@ -50,7 +55,8 @@ func (sh *SerialHandler) connect() bool {
 
 }
 
-func (sh *SerialHandler) listen(chn chan string) {
+// listenAndReconnect listens for serial data and infinitly tries to reconnect
+func (s *Sphincter) listenAndReconnect(chn chan string) {
 
 	go func(chn chan string) {
 
@@ -61,13 +67,14 @@ func (sh *SerialHandler) listen(chn chan string) {
 
 		// loop for reconnecting
 		for {
-			if sh.connect() {
+			if s.connect() {
+				defer s.Close()
 
-				defer sh.serial_con.Close()
+				// FIXME: flush buffer
 
 				// listen for data
 				for {
-					n, err = sh.serial_con.Read(buf)
+					n, err = s.Read(buf)
 
 					if err != nil {
 						log.Println(err)
@@ -78,11 +85,7 @@ func (sh *SerialHandler) listen(chn chan string) {
 					// see http://arduino.cc/en/Serial/Println
 					out += string(buf[:n])
 					if n > 0 && buf[n-1] == '\n' {
-						out = strings.Trim(out, "\r\n")
-
-						sh.onDataReceived(out)
-						chn <- out
-
+						chn <- strings.Trim(out, "\r\n")
 						out = ""
 					}
 				}
@@ -93,23 +96,9 @@ func (sh *SerialHandler) listen(chn chan string) {
 	}(chn)
 }
 
-// onDataReceived can be used to trigger actions on a state change
-func (sh *SerialHandler) onDataReceived(data string) {
+func (s *Sphincter) performAction(action string) {
 
-	// FIXME: add beehive/spaceapi call
-	switch data {
-	case RESPONSE_ACK:
-	case RESPONSE_LOCKED:
-	case RESPONSE_UNLOCKED:
-	case RESPONSE_OPEN:
-	case RESPONSE_UNKNOWN:
-	default:
-	}
-}
-
-func (sh *SerialHandler) performAction(action string) {
-
-	_, err := sh.serial_con.Write([]byte(action))
+	_, err := s.Write([]byte(action))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -118,35 +107,47 @@ func (sh *SerialHandler) performAction(action string) {
 
 func main() {
 
-	serial_handler := SerialHandler{"/dev/pts/2", 9600, nil}
+	sphincter := Sphincter{
+		"/dev/pts/4",
+		9600,
+		nil}
+
+	var httpRespQueue []*chan string
 	serial_chn := make(chan string)
 
-	// start serial main loop
-	serial_handler.listen(serial_chn)
+	sphincter.listenAndReconnect(serial_chn)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		err := r.ParseForm()
-		if err != nil {
-		}
+		// TODO check auth
 
-		var action string
+		if err := r.ParseForm(); err != nil {
+		}
 
 		switch r.Form.Get("action") {
-		case "open":
-			action = ACTION_OPEN
-		case "close":
-			action = ACTION_CLOSE
-		case "state":
-			action = ACTION_STATE
+		case ACN_CLOSE, ACN_OPEN, ACN_STATE:
+			chn := make(chan string)
+			httpRespQueue = append(httpRespQueue, &chn)
+
+			// wait for corresponding serial response
+			fmt.Fprint(w, <-chn)
 		default:
+			fmt.Fprint(w, "INVALID ACTION")
+			return
 		}
 
-		serial_handler.performAction(action)
-
-		fmt.Fprint(w, <-serial_chn)
 	})
+	go func() { http.ListenAndServe(":8080", nil) }()
 
-	func() { log.Fatal(http.ListenAndServe(":8080", nil)) }()
+	// daemon main loop
+	for {
+		// wait for serial data
+		serial_data := <-serial_chn
 
+		// check if there are waiting http connections
+		if len(httpRespQueue) > 0 {
+			*httpRespQueue[0] <- serial_data
+			httpRespQueue = httpRespQueue[1:]
+		}
+	}
 }
